515a728b821f55b18818a6afb375d1ee9fb334f6
Christophe Robin <crobin@nekoo.com>
2013-04-22 13:11:40 +0900

    Ran go fmt on the go source

--- go/mtgox/mtgox.go
+++ go/mtgox/mtgox.go
@@ -1,47 +1,48 @@
 package mtgox
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/base64"
 	"fmt"
-    "io/ioutil"
-    "encoding/base64"
-    "crypto/hmac"
-    "crypto/sha512"
+	"io/ioutil"
 	"net/http"
-    "net/http/httputil"
+	"net/http/httputil"
 	"net/url"
 	"strconv"
-    "strings"
+	"strings"
 	"time"
 )
 
 type AuthType int
+
 const (
-    Auto AuthType = iota
-    Public
-    Private
+	Auto AuthType = iota
+	Public
+	Private
 )
 
 type Api struct {
 	ApiKey    string
 	ApiSecret string
-    Auth      AuthType
-    Debug     bool
+	Auth      AuthType
+	Debug     bool
 }
 
 type Request struct {
 	HttpRequest *http.Request
-    Path        string
+	Path        string
 	Parameters  url.Values
 }
 
 type InvalidVersion int8
 
 func (version InvalidVersion) Error() string {
-    return fmt.Sprintf("Invalid API version %d", int(version))
+	return fmt.Sprintf("Invalid API version %d", int(version))
 }
 
 func NewApi(key, secret string) Api {
-    return Api{key, secret, Auto, false}
+	return Api{key, secret, Auto, false}
 }
 
 func parseParameters(params map[string]interface{}) url.Values {
@@ -73,62 +74,65 @@ func parseParameters(params map[string]interface{}) url.Values {
 }
 
 func (api Api) Public() Api {
-    api.Auth = Public
-    return api
+	api.Auth = Public
+	return api
 }
 
 func (api Api) Private() Api {
-    api.Auth = Private
-    return api
+	api.Auth = Private
+	return api
 }
 
 func (api Api) Auto() Api {
-    api.Auth = Auto
-    return api
+	api.Auth = Auto
+	return api
 }
 
 func (api Api) auth(request *Request) error {
-    // extract api version number
-    version, err := strconv.ParseInt(request.Path[0:1], 10, 8)
-    if err != nil { return err }
-    if version < 0 || version > 2 {
-        return InvalidVersion(version)
-    }
+	// extract api version number
+	version, err := strconv.ParseInt(request.Path[0:1], 10, 8)
+	if err != nil {
+		return err
+	}
+	if version < 0 || version > 2 {
+		return InvalidVersion(version)
+	}
 
-    // add the nonce value to the request parameters
-    request.Parameters.Add("nonce", strconv.FormatInt(time.Now().UnixNano(), 10))
+	// add the nonce value to the request parameters
+	request.Parameters.Add("nonce", strconv.FormatInt(time.Now().UnixNano(), 10))
 
-    // first decode secret
-    b64 := base64.StdEncoding
-    secret, err := b64.DecodeString(api.ApiSecret)
-    if err != nil { return err }
+	// first decode secret
+	b64 := base64.StdEncoding
+	secret, err := b64.DecodeString(api.ApiSecret)
+	if err != nil {
+		return err
+	}
 
-    // the data we want to encode
-    encodedData := request.Parameters.Encode()
-    if version == 2 {
-        // version 2 add the pathname for more entropy
-        encodedData = request.Path[2:] + "\x00" + encodedData
-    }
+	// the data we want to encode
+	encodedData := request.Parameters.Encode()
+	if version == 2 {
+		// version 2 add the pathname for more entropy
+		encodedData = request.Path[2:] + "\x00" + encodedData
+	}
 
-    // retrieve a sha512 hmac generator
-    hmac := hmac.New(sha512.New, secret)
-    // write our encoded data
-    hmac.Write([]byte(encodedData))
-    signature := b64.EncodeToString(hmac.Sum(nil))
+	// retrieve a sha512 hmac generator
+	hmac := hmac.New(sha512.New, secret)
+	// write our encoded data
+	hmac.Write([]byte(encodedData))
+	signature := b64.EncodeToString(hmac.Sum(nil))
 
-    // add headers to request
-    request.HttpRequest.Header.Add("Rest-Key", api.ApiKey)
-    request.HttpRequest.Header.Add("Rest-Sign", signature)
+	// add headers to request
+	request.HttpRequest.Header.Add("Rest-Key", api.ApiKey)
+	request.HttpRequest.Header.Add("Rest-Sign", signature)
 
-    return nil
+	return nil
 }
-
 
 func (api Api) Call(path string, params map[string]interface{}) (string, error) {
 	if params == nil {
 		params = map[string]interface{}{}
 	}
-    // remove leading slashes in path
+	// remove leading slashes in path
 	path = strings.TrimLeft(path, "/")
 	// setup parameter list
 	parameters := parseParameters(params)
@@ -136,50 +140,64 @@ func (api Api) Call(path string, params map[string]interface{}) (string, error) 
 	// create request
 	request := Request{nil, path, parameters}
 
-    // default call URL
-    url, err := url.Parse("https://data.mtgox.com/api/" + path)
-    if err != nil { return "", err }
+	// default call URL
+	url, err := url.Parse("https://data.mtgox.com/api/" + path)
+	if err != nil {
+		return "", err
+	}
 
-    // do auth only if key+secret are provided
-    if api.Auth == Private || (api.Auth == Auto && api.ApiKey != "" && api.ApiSecret != "") {
-        // because we need to authenticate, data.mtgox.com won't work
-        url.Host = "mtgox.com"
-        request.HttpRequest, err = http.NewRequest("POST", url.String(), nil)
-        if err != nil { return "", err }
-        // then run auth
-        err := api.auth(&request)
-        if err != nil { return "", err }
-        // kinda hackish, but well
-        postBuffer := strings.NewReader(request.Parameters.Encode())
-        request.HttpRequest.Body = ioutil.NopCloser(postBuffer)
-        request.HttpRequest.ContentLength = int64(postBuffer.Len())
-        // don't forget to setup header for POST data
-        request.HttpRequest.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-    } else {
-        // if we have any parameters, encode them in the URI
-        if len(parameters) > 0 {
-            url.RawQuery = parameters.Encode()
-        }
-        request.HttpRequest, err = http.NewRequest("GET", url.String(), nil)
-        if err != nil { return "", err }
-    }
+	// do auth only if key+secret are provided
+	if api.Auth == Private || (api.Auth == Auto && api.ApiKey != "" && api.ApiSecret != "") {
+		// because we need to authenticate, data.mtgox.com won't work
+		url.Host = "mtgox.com"
+		request.HttpRequest, err = http.NewRequest("POST", url.String(), nil)
+		if err != nil {
+			return "", err
+		}
+		// then run auth
+		err := api.auth(&request)
+		if err != nil {
+			return "", err
+		}
+		// kinda hackish, but well
+		postBuffer := strings.NewReader(request.Parameters.Encode())
+		request.HttpRequest.Body = ioutil.NopCloser(postBuffer)
+		request.HttpRequest.ContentLength = int64(postBuffer.Len())
+		// don't forget to setup header for POST data
+		request.HttpRequest.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	} else {
+		// if we have any parameters, encode them in the URI
+		if len(parameters) > 0 {
+			url.RawQuery = parameters.Encode()
+		}
+		request.HttpRequest, err = http.NewRequest("GET", url.String(), nil)
+		if err != nil {
+			return "", err
+		}
+	}
 
-    // debug stuff
-    if api.Debug {
-        fmt.Println("Output:")
-        dump, err := httputil.DumpRequestOut(request.HttpRequest, true)
-        if err != nil { return "", err }
-        fmt.Println(string(dump))
-    }
+	// debug stuff
+	if api.Debug {
+		fmt.Println("Output:")
+		dump, err := httputil.DumpRequestOut(request.HttpRequest, true)
+		if err != nil {
+			return "", err
+		}
+		fmt.Println(string(dump))
+	}
 
-    // now do the query
-    client := &http.Client{}
-    resp, err := client.Do(request.HttpRequest)
-    if err != nil { return "", err }
+	// now do the query
+	client := &http.Client{}
+	resp, err := client.Do(request.HttpRequest)
+	if err != nil {
+		return "", err
+	}
 
-    // everything is good! read the response
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil { return "", err }
+	// everything is good! read the response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
 
 	return string(body), nil
 }
--- go/test.go
+++ go/test.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"./mtgox"
-    "flag"
+	"encoding/json"
+	"flag"
 	"log"
-    "os"
-    "encoding/json"
+	"os"
 )
 
 var flagKey = flag.String("key", "", "your api key")
@@ -13,56 +13,59 @@ var flagSecret = flag.String("secret", "", "your api secret")
 
 // used for .apirc
 type Credentials struct {
-    Key string
-    Secret string
+	Key    string
+	Secret string
 }
 
 func main() {
-    var key, secret string
+	var key, secret string
 
-    // small prefix for output
-    log.SetPrefix("mtgox-go ")
+	// small prefix for output
+	log.SetPrefix("mtgox-go ")
 
-    // parse command-line
-    flag.Parse()
+	// parse command-line
+	flag.Parse()
 
-    if *flagKey == "" || *flagSecret == "" {
-        // check if .apirc exists in the main path, and load credentials from it if necessary
-        file, err := os.Open("../.apirc")
-        if err == nil {
-            // file is here, parse it
-            dec := json.NewDecoder(file)
-            var cred Credentials
-            if err := dec.Decode(&cred); err == nil {
-                key = cred.Key
-                secret = cred.Secret
-            }
-            file.Close()
-        }
-    } else {
-        key = *flagKey
-        secret = *flagSecret
-    }
-
+	if *flagKey == "" || *flagSecret == "" {
+		// check if .apirc exists in the main path, and load credentials from it if necessary
+		file, err := os.Open("../.apirc")
+		if err == nil {
+			// file is here, parse it
+			dec := json.NewDecoder(file)
+			var cred Credentials
+			if err := dec.Decode(&cred); err == nil {
+				key = cred.Key
+				secret = cred.Secret
+			}
+			file.Close()
+		}
+	} else {
+		key = *flagKey
+		secret = *flagSecret
+	}
 
 	// instanciate mtgox Api
 	api := mtgox.NewApi(key, secret)
 	// call test Api, by adding Public() we disable auth and hit on data.mtgox.com (faster)
 	result, err := api.Public().Call("2/BTCUSD/money/ticker", nil)
 	// catch errors
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	// give us the output
 	log.Println(result)
 
-    // do the same with the private API if a key/secret was found
-    if key != "" {
-        // now the private API, because Public() create a copy of the api, we don't need to
-        // call Private() for this call, the class will detect that a key/secret are set and
-        // will automatically sign the request
-        result, err = api.Call("2/money/info", nil)
-        // catch errors
-        if err != nil { log.Fatal(err) }
-        // give us the output
-        log.Println(result)
-    }
+	// do the same with the private API if a key/secret was found
+	if key != "" {
+		// now the private API, because Public() create a copy of the api, we don't need to
+		// call Private() for this call, the class will detect that a key/secret are set and
+		// will automatically sign the request
+		result, err = api.Call("2/money/info", nil)
+		// catch errors
+		if err != nil {
+			log.Fatal(err)
+		}
+		// give us the output
+		log.Println(result)
+	}
 }
